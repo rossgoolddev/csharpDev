9a34f560f0313af1b71269985ee419a15d11f2d1
rossgoolddev <rossgoolddev@gmail.com>
2021-12-21 17:20:45 +0000

    First test
    Got the debug to work. It is Debug.WriteLine not Console.WriteLine

---
+++ Projects/ClassesDevelopment/ClassesDev/ClassesDev/Classes/People.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesDev.Classes
+{
+    class People
+    {
+        public string eyeColour = "blue";
+        public int age = 10;
+        double exactAge = 10.4;
+        char firstInitial = 'R';
+        char lastInitial = 'G';
+        bool smelly = true;
+    }
+}
---
+++ Projects/ClassesDevelopment/ClassesDev/ClassesDev/Classes/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace ClassesDev.Classes
+{
+    class Person
+    {
+        static void Main(string[] args)
+        {
+            People myPeople = new People();
+            Debug.WriteLine("I know why...but WHYYYY " + myPeople.age); // has to be debug.write line to come up!
+        }
+    }
+}
